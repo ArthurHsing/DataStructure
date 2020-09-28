c376f4ba55bc4a3e15c42c9b9252822f0ca01fc4
幸成 <573105494@qq.com>
2020-09-28 20:24:36 +0800

    Use symbol as the variable name

---
+++ 3.Stack_symbol.js
@@ -0,0 +1,44 @@
+const items = Symbol('stackItems');
+class Stack {
+  constructor() {
+    this[items] = {};
+    this.count = 0;
+  }
+  push(element) {
+    this[items][this.count++] = element;
+  }
+  size() {
+    return this.count;
+  }
+  isEmpty() {
+    return this.count === 0;
+  }
+  pop() {
+    if (!this.isEmpty()) {
+      const result = this[items][this.count - 1];
+      delete this[items][--this.count];
+      return result;
+    }
+    return undefined;
+  }
+  peek() {
+    if (!this.isEmpty()) {
+      return this[items][this.count - 1];
+    }
+    return undefined;
+  }
+  clear() {
+    this[items] = {};
+    this.count = 0;
+  }
+  toString() {
+    if (!this.isEmpty()) {
+      let objString = `${this[items][0]}`;
+      for (let i = 1; i < this.count; i++) {
+        objString = `${objString}, ${this[items][i]}`;
+      }
+      return objString;
+    }
+    return '';
+  }
+}
