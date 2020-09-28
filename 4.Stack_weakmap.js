bd1ce0607dc54ca7b85ba7b47fa67f136e0fa879
幸成 <573105494@qq.com>
2020-09-28 20:43:30 +0800

    use weakmap to realize private properties of stack

---
+++ 4.Stack_weakmap.js
@@ -0,0 +1,29 @@
+const wm = new WeakMap();
+class Stack {
+  constructor() {
+    wm.set(this, []);
+  }
+  push(element) {
+    const arr = wm.get(this);
+    arr.push(element);
+  }
+  pop() {
+    const arr = wm.get(this);
+    return arr.pop();
+  }
+  peek() {
+    const arr = wm.get(this);
+    return arr[arr.length - 1];
+  }
+  isEmpty() {
+    const arr = wm.get(this);
+    return arr.length === 0;
+  }
+  size() {
+    const arr = wm.get(this);
+    return arr.length;
+  }
+  clear() {
+    wm.set(this, []);
+  }
+}
