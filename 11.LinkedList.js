d040f9c7aab8c5c1dad47688210c36b11bc090fa
幸成 <573105494@qq.com>
2020-09-30 21:48:57 +0800

    LinkedList v1

---
+++ 11.LinkedList.js
@@ -0,0 +1,56 @@
+import { defaultEquals } from './util.js'
+import { Node } from './models/linked-list-models.js'
+export default class LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    this.count = 0;
+    this.head = undefined;
+    this.equalsFn = equalsFn;
+  }
+  push(element) {
+    const node = new Node(element);
+    if (this.head == null) {
+      this.head = node
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.count++;
+  }
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        this.head = current.next;
+      } else {
+        let previous;
+        for (let i = 0; i < index; i++) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+    return undefined;
+  }
+  getElementAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current.next;
+      }
+      return current;
+    }
+    return undefined;
+  }
+}
+
+const linkedList = new LinkedList();
+linkedList.push(1);
+linkedList.push(2);
+linkedList.push(3);
+console.log(linkedList);
