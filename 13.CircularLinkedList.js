73a7140fbcfaac37e10ea88596e0327dafc65580
幸成 <573105494@qq.com>
2020-10-01 17:35:16 +0800

    Circular linked list v2

--- 13.CircularLinkedList.js
+++ 13.CircularLinkedList.js
@@ -7,11 +7,10 @@ class CircularLinkedList extends LinkedList {
   }
   insert(element, index) {
     if (index >= 0 && index <= this.count) {
-      let node = new Node(element);
+      const node = new Node(element);
       if (index === 0) {
         if (this.head == null) {
-          this.head = node;
-          this.head.next = this.head;
+          this.push(element);
         } else {
           node.next = this.head;
           this.head = node;
@@ -27,11 +26,49 @@ class CircularLinkedList extends LinkedList {
     }
     return false;
   }
+  push(element) {
+    const node = new Node(element);
+    if (this.count === 0) {
+      this.head = node;
+      this.head.next = this.head;
+    } else {
+      let current = this.head;
+      for (let i = 1; i < this.count; i++) {
+        current = current.next;
+      }
+      current.next = node;
+      node.next = this.head;
+    }
+    this.count++;
+    return true;
+  }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let deleted;
+      if (index === 0) {
+        deleted = this.head;
+        if (this.count === 1) {
+          this.head = undefined;
+        } else {
+          const last = this.getElementAt(this.count - 1);
+          this.head = this.head.next;
+          last.next = this.head;
+        }
+      } else {
+        const previous = this.getElementAt(index - 1);
+        deleted = previous.next;
+        previous.next = deleted.next;
+      }
+      return deleted;
+    }
+  }
 }
 
 const c = new CircularLinkedList();
-c.insert(1, 0);
-c.insert(2, 1);
-c.insert(3, 2);
-c.insert('1-2', 3);
+c.push(1);
+c.push(2);
+c.push(3);
+c.push(4);
+c.removeAt(2)
 console.dir(c);
