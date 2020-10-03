2013ef0a318c878070ce9ed6c69860c25224a46f
幸成 <573105494@qq.com>
2020-10-03 19:27:55 +0800

    Set intersection

--- 16.Set.js
+++ 16.Set.js
@@ -42,15 +42,35 @@ class Set {
     });
     return unionSet;
   }
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+    let bigger = this.valuesLegacy();
+    let smaller = otherSet.valuesLegacy();
+    (bigger.length < smaller.length) && ([bigger, smaller] = [smaller, bigger]);
+    console.log(bigger);
+    for (let i of smaller) {
+      if (bigger.includes(i)) {
+        intersectionSet.add(i);
+      }
+    }
+    return intersectionSet;
+  }
 }
 
 const set1 = new Set();
 set1.add(1);
 set1.add(2);
 set1.add(3);
+set1.add(4);
+set1.add(5);
+set1.add(6);
+set1.add(7);
+set1.add(8);
 
 const set2 = new Set();
 set2.add(2);
 set2.add(3);
-set2.add(4);
+set2.add(9);
+
 console.log(set1.union(set2).valuesLegacy());
+console.log(set1.intersection(set2).valuesLegacy());
