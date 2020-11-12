398a7cb6a52e91aa6085020d64a0bbda060eb60f
幸成 <573105494@qq.com>
2020-11-12 20:55:02 +0800

    lcs first

---
+++ 47.lcs.js
@@ -0,0 +1,29 @@
+const lcs = (wordX, wordY) => {
+  const m = wordX.length;
+  const n = wordY.length;
+  const l = [];
+  for (let i = 0; i <= m; i++) {
+    l[i] = [];
+    for (let j = 0; j <= n; j++) {
+      l[i][j] = 0;
+    }
+  }
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      if (i === 0 || j === 0) {
+        l[i][j] = 0;
+      } else if (wordX[i - 1] === wordY[j - 1]) {
+        l[i][j] = l[i - 1][j - 1] + 1;
+      } else {
+        const a = l[i - 1][j];
+        const b = l[i][j - 1];
+        l[i][j] = a > b ? a : b;
+      }
+    }
+  }
+  console.log(l);
+  return l[m][n];
+}
+const str1 = 'string';
+const str2 = 'string';
+console.log(lcs(str1, str2));
