4cf2a0c944d408be55fbbe5304739df0cb1c3edf
幸成 <573105494@qq.com>
2020-11-14 09:10:21 +0800

    minCoinChange change

--- 45.minCoinChange.js
+++ 45.minCoinChange.js
@@ -1,29 +1,29 @@
-const minCoin = (coins, amount) => {
-  let cache = [];
-  const coinChange = (value) => {
+const minCoinChange = (coins, amount) => {
+  const cache = [];
+  const change = (value) => {
     if (!value) {
-      return []
+      return [];
     }
     if (cache[value]) {
       return cache[value];
     }
-    let minCoins = [];
-    let newAmount;
-    let newCoin;
-    for (let i = 0; i < coins.length; i++) {  //每个硬币代表一种情况，比较哪一种情况下的硬币数量最少，每一种情况通过递归迭代来找到最小的值
+    let mins = [];
+    let newMin; //代表去掉减去当前硬币的面额，剩余钱的总数找零的最小值
+    for (let i = 0; i < coins.length; i++) {//每个硬币代表一种情况，比较哪一种情况下的硬币数量最少，每一种情况通过递归迭代来找到最小的值
       const coin = coins[i];
-      newAmount = value - coin;
-      if (newAmount >= 0) {
-        newCoin = coinChange(newAmount);
-      }
-      if (newAmount >= 0 && (newCoin.length < minCoins.length - 1 || !minCoins.length)) {
-        minCoins = [coin].concat(newCoin);
+      const remain = value - coin;
+      if (remain >= 0) {
+        newMin = change(remain);
+        if (newMin.length < mins.length - 1 || !mins.length) {
+          mins = [coin].concat(newMin);
+        }
       }
     }
-    return (cache[value] = minCoins);
+    return (cache[value] = mins);
   }
-  return coinChange(amount);
+  return change(amount);
 }
 
-const arr = [1, 2, 5, 10, 20, 50];
-console.log(minCoin(arr, 89));
+const coins = [1, 2, 5, 10];
+const amount = 24;
+console.log(minCoinChange(coins, amount));
