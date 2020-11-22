e0006c773a766129c71309580541a71f00c23193
幸成 <573105494@qq.com>
2020-11-22 10:54:18 +0800

    floyd improve

--- 31.Prim.js
+++ 31.Prim.js
@@ -11,8 +11,8 @@ const minKey = (key, visited) => {
   return minIndex;
 }
 const prim = (graph) => {
-  const key = []; //用来保存到父节点的距离
-  const parent = [];  //用来保存父节点是谁
+  const key = []; //用来保存当前节点的所有未加入生成树的父节点（相邻节点）到当前节点的最小距离
+  const parent = [];  //用来保存当前节点的父节点是谁
   const visited = []; //用来保存某个点是否已经加入了最小生成树中
   const length = graph.length;
   for (let i = 0; i < length; i++) {
@@ -21,7 +21,7 @@ const prim = (graph) => {
   }
   parent[0] = -1;
   key[0] = 0;
-  for (let i = -1; i < length - 1; i++) {
+  for (let i = 0; i < length - 1; i++) {
     const u = minKey(key, visited);
     visited[u] = true;
     for (let v = 0; v < length; v++) {
