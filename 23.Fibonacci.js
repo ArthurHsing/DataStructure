f4f87457d5bacebe2358d57300d2cdc7995eae93
幸成 <573105494@qq.com>
2020-12-02 10:55:45 +0800

    fibonacci tail call optimization

--- 23.Fibonacci.js
+++ 23.Fibonacci.js
@@ -1,19 +1,43 @@
-function fibonacci(n) {
-  if (n < 1) return 0;
-  if (n <= 2) return 1;
-  const result = fibonacci(n - 1) + fibonacci(n - 2);
-  console.log(result);
-  return result;
-}
+"use strict"
+console.time('执行时间');
+function fib(n) {
+  const result = fibTool(0n, 1n, BigInt(n));
+  return Number(result % BigInt(1e9 + 7));
+};
 
-function fibonacciMemoization(n) {
-  const memo = [0, 1];
-  const fibonacci = (n) => {
-    if (memo[n] != null) return memo[n];
-    return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+function fibTool(a, b, n) {
+  if (n === 0n) {
+    return a;
   }
-  fibonacci(n);
-  console.log(memo);
+  return fibTool(b, a + b, n - 1n);
 }
 
-console.log(fibonacciMemoization(7));
+console.log(fib(40));
+console.timeEnd('执行时间');
+/**
+ * 迭代版本的fibonacci数列
+ */
+/*
+console.time('执行开始');
+function fibonacci(n) {
+  return Number(fibonacciTool(BigInt(n)));
+}
+function fibonacciTool(n) {
+  if (n === 0n) {
+    return 0n;
+  }
+  if (n === 1n) {
+    return 1n;
+  }
+  let a = 0n;
+  let b = 1n;
+  for (let i = 2n; i <= n; i += 1n) {
+    const temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
+console.log(fibonacci(40));
+console.timeEnd('执行开始');
+*/
