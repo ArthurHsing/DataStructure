173966e203378c8a5c8fe78c859d374b3f361f9a
幸成 <573105494@qq.com>
2020-12-24 09:28:46 +0800

    better understanding about quick sort

--- 37.QuickSort.js
+++ 37.QuickSort.js
@@ -29,6 +29,53 @@ function quick(arr, left, right) {
 function quickSort(arr) {
   quick(arr, 0, arr.length - 1);
 }
+
+// 下面这种不加=的效率要高一点点，少循环一次
+/**
+function partition(arr, left, right) {
+  const pivot = arr[Math.floor((left + right) / 2)];
+  while (left <= right) {
+    while (arr[left] < pivot) {
+      left++;
+    }
+    while (arr[right] > pivot) {
+      right--;
+    }
+    if (left <= right) {
+      [arr[left], arr[right]] = [arr[right], arr[left]];
+      left++;
+      right--;
+    }
+  }
+  return left;
+}
+
+function quick(arr, left, right) {
+  let index = partition(arr, left, right);
+  if (index - 1 > left) {
+    quick(arr, left, index - 1);
+  }
+  if (index < right) {
+    quick(arr, index, right);
+  }
+}
+
+function quickSort(arr) {
+  quick(arr, 0, arr.length - 1);
+}
+
+ */
+
+
+
+
+
+
+
+
+
+
+
 const arr = [3, 5, 1, 6, 4, 7, 2];
 quickSort(arr, 0, arr.length - 1);
 console.dir(arr);
